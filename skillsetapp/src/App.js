ed9ad4c76fab7c5ed1a4b6d2b01b4c4a2a75a646
USER <thamidwane27@icloud.com>
2024-10-25 12:13:52 +0200

    About

--- skillsetapp/src/App.js
+++ skillsetapp/src/App.js
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 import React from 'react';
 import Home from './Home/Home';
 import Navbar from './Navbar/Navbar';
-=======
+
 
 import './App.css';
 
->>>>>>> f4567d2f1289ff7656b0e5e940b989c0959b3b43
 function App() {
   let Component;
   switch (window.location.pathname.toLowerCase()) {
