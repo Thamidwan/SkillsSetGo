e6a1fdec53fb9d65947d28eb9c8bb8e7e901bed8
Musa <royaldesire10@gmail.com>
2024-10-26 17:48:11 +0200

    saving skills

--- skillsetapp/src/Skills/Skills.js
+++ skillsetapp/src/Skills/Skills.js
@@ -1,76 +1,34 @@
 import React from "react";
-import './Skills.css'
+import './Skills.css';
 
 function Skills() {
     return (
-<<<<<<< HEAD
-        <div class="contain">
-            <div class="">
+        <div className="container">
+            <div className="header">
                 <h1>Master In-Demand Skills Today</h1>
-                <p>"Sed ut perspiciatis unde omnis iste
-                 natus error sit voluptatem accusantium 
-                 doloremque laudantium, totam rem aperiam,
-                  eaque ipsa quae ab illo inventore 
-                  veritatis et quasi architecto beatae 
-                  vitae dicta sunt explicabo. Nemo enim ipsam 
-                  voluptatem quia voluptas sit aspernatur 
-                  aut odit aut fugit, sed quia consequuntur
-                  magni dolores eos qui ratione</p>
+                <p>
+                    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                    veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                    voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                    magni dolores eos qui ratione"
+                </p>
             </div>
-            <div>
-                
-           </div>
-            <div class="table-container">
-                <table>
-                    <tr>
-                        <td>Fundamental of Software skills</td>
-                        <td>Introduction to trending technologies</td>
-                        <td>Tutorials in web development</td>
-                    </tr>
-                    <tr>
-                        <td><button class="blue-button">Explore more</button></td>
-                        <td><button class="blue-button">Beginner's Level</button></td>
-                        <td><button class="blue-button">Beginner's Level</button></td>
-                    </tr>
-                </table>
-=======
-        <div class="container">
-            <div class="header">
-                <h1>Master In-Demand Skills Today</h1>
-            </div>
-            <div class="paragraph">
-                <p>The information that is needed according to this screenshot. Unfortunately I don't understand the language and therefore I cannot put anything here.</p>
-            </div>
-            {/* <div class="table-container">
-                <div class="outer">
-                    <div>
-                        <p>Fundmental of Software skills</p>
-                        <p>Introduction to trending technologies</p>
-                        <p>Tutorials in web development</p>
-                    </div>
-                    <div>
-                        <button>Explore more</button>
-                        <button>Beginner's Level</button>
-                        <button>Beginner's Level</button>
-                    </div>
-                </div>
-            </div> */}
 
-            <div class="containing">
-                <div class="contentz">
-                    <div class="item">Fundamental of Software skills</div>
-                    <div class="item">Introduction to trending technologies</div>
-                    <div class="item">Tutorials in web development</div>
+            <div className="table-container">
+                <div className="content">
+                    <div className="item">Fundamental of Software Skills</div>
+                    <div className="item">Introduction to Trending Technologies</div>
+                    <div className="item">Tutorials in Web Development</div>
                 </div>
-                <div class="buttons">
-                    <button class="blue-button">Explore more</button>
-                    <button class="blue-button">Beginner's Level</button>
-                    <button class="blue-button">Beginner's Level</button>
+                <div className="buttons">
+                    <button className="blue-button">Explore More</button>
+                    <button className="blue-button">Beginner's Level</button>
+                    <button className="blue-button">Beginner's Level</button>
                 </div>
->>>>>>> 9e0109bed51853bc9f29e79d175b25dff407bd58
             </div>
         </div>
-    )
+    );
 }
 
-export default Skills
+export default Skills;
