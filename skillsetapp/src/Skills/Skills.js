9d4d09258c5c7c59e7cbd08b86ad8fa8ec292702
Paul Tebeila <paultebeila@gmail.com>
2024-10-26 12:32:46 +0200

    First commit

---
+++ skillsetapp/src/Skills/Skills.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './Skills.css'
+
+function Skills() {
+    return (
+        <div class="container">
+            <div class="header">
+                <h2>Master In-Demand Skills Today</h2>
+            </div>
+            <div>
+                <p>The information that is needed according to this screenshot. Unfortunately I don't understand the language and therefore I cannot put anything here.</p>
+            </div>
+            <div class="table-container">
+                <table>
+                    <tr>
+                        <td>Fundamental of Software skills</td>
+                        <td>Introduction to trending technologies</td>
+                        <td>Tutorials in web development</td>
+                    </tr>
+                    <tr>
+                        <td><button class="blue-button">Explore more</button></td>
+                        <td><button class="blue-button">Beginner's Level</button></td>
+                        <td><button class="blue-button">Beginner's Level</button></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
